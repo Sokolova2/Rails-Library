6279abb13a33a31fb5dfc3804fef295f9e239d36
Karina <karinasokolova252@gmail.com>
2025-09-30 20:18:07 +0300

    Add uploader for book images

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,25 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def size_range
+    (10.kilobytes)..(50_000.megabytes)
+  end
+
+  version :thumb do
+    process resize_to_fit: [100, 200]
+  end
+
+  version :medium do
+    process resize_to_fit: [300, 400]
+  end
+
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
+end
