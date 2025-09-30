872cfb30d7dc246751dda2ce3779cd39be49fb03
Karina <karinasokolova252@gmail.com>
2025-09-30 18:20:02 +0300

    Add new attribute for avatar for registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: %i[create]
+  before_action :configure_account_update_params, only: %i[update]
 
   protected
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:login, :language])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[login avatar language])
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:login, :language])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[login avatar language])
   end
 
   def after_sign_up_path_for(resource)
