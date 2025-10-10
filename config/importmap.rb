1c7f11fe85188c1ae0fbaaf79f5749518252fc91
Karina <karinasokolova252@gmail.com>
2025-10-10 20:07:15 +0300

    Add imports for stimulus

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,6 @@ pin 'application'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@popperjs/core', to: 'popper.js', preload: true
 pin 'bootstrap', to: 'bootstrap.bundle.min.js', preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
