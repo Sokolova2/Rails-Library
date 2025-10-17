a88e37020cfc9a3515e893413ff354152c54fe5e
Karina <karinasokolova252@gmail.com>
2025-10-17 22:02:45 +0300

    Separeted method

--- app/services/update_book_status_service.rb
+++ app/services/update_book_status_service.rb
@@ -7,26 +7,48 @@ class UpdateBookStatusService
   end
 
   def update_book_status
-    # TODO: change name
-    # TODO Move user favorite logic to separate method
-    book_favorites = Favorite.where(book_id: @book.id)
-
-    users = book_favorites.pluck(:user_id)
-
-    @users_favorite = User.in(id: users)
-
     if @book.status == 'Open'
       book_open
+      create_history
+      message_close_book
     elsif @book.status == 'Closed'
       book_close
+      update_history
+      message_open_book
     end
   end
 
+  def users_favorite_books
+    book_favorites = Favorite.where(book_id: @book.id)
+
+    users_favorites = book_favorites.pluck(:user_id)
+
+    @users = User.in(id: users_favorites)
+  end
+
   def book_open
+    users_favorite_books
+
     @book.update(status: 'Closed', user_id: @user.id)
-    # TODO: move history to another method
+  end
+
+  def book_close
+    users_favorite_books
+
+    @book.update(status: 'Open', user_id: nil)
+  end
+
+  def create_history
     History.create(user_id: @user.id, book_id: @book.id, status: 'took')
-    @users_favorite.each do |u|
+  end
+
+  def update_history
+    history = History.where(book_id: @book.id).last
+    history.update(status: 'return')
+  end
+
+  def message_close_book
+    @users.each do |u|
       Message.create(
         user_id: u.id,
         content: "Book \"#{@book.title}\" status updated. The book from your favorite is now closed for use"
@@ -34,11 +56,8 @@ class UpdateBookStatusService
     end
   end
 
-  def book_close
-    @book.update(status: 'Open', user_id: nil)
-    history = History.where(book_id: @book.id).last
-    history.update(status: 'return')
-    @users_favorite.each do |u|
+  def message_open_book
+    @users.each do |u|
       Message.create(
         user_id: u.id,
         content: "Book \"#{@book.title}\" status updated. The book from your favorite is now open for use again"
