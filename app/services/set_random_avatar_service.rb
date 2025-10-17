4a35aa045ccf9ec621b11cd32a1be41ef8aa17cd
Karina <karinasokolova252@gmail.com>
2025-10-17 22:36:13 +0300

    Move method for set avatar with gender in separate method

--- app/services/set_random_avatar_service.rb
+++ app/services/set_random_avatar_service.rb
@@ -8,21 +8,24 @@ class SetRandomAvatarService
   def set_random_avatar
     return if @user.avatar.present?
 
-    if @user.gender.nil?
+    if @user.gender.nil? || @user.gender == 'gender'
       set_avatar_without_gender
-      return
+    else
+      set_avatar_with_gender
     end
+  end
 
-    gender = @user.gender == 'female' ? 'female' : 'male'
-    avatar = Rails.root.glob("app/assets/images/#{gender}/*.png")
+  def set_avatar_without_gender
+    avatar = Rails.root.glob('app/assets/images/animal/*.png')
     random_avatar = avatar.sample
 
     @user.avatar = File.open(random_avatar)
     @user.avatar_filename = File.basename(random_avatar)
   end
 
-  def set_avatar_without_gender
-    avatar = Rails.root.glob('app/assets/images/animal/*.png')
+  def set_avatar_with_gender
+    gender = @user.gender == 'female' ? 'female' : 'male'
+    avatar = Rails.root.glob("app/assets/images/#{gender}/*.png")
     random_avatar = avatar.sample
 
     @user.avatar = File.open(random_avatar)
